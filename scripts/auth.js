a5438c3b5c6b3891bf0f9f39f04508ecaa2aba85
amirhosseinlangari <amirhossseinlangari@gmail.com>
2025-06-02 18:30:42 +0330

    Update

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,7 +5,7 @@
 
 class AuthManager {
     constructor() {
-        this.baseUrl = 'https://api.kabancabinet.com/api';
+        this.baseUrl = 'http://localhost:5000/api';
         this.tokenKey = 'auth_token';
         this.userKey = 'user_data';
         this.sessionKey = 'session_id';
