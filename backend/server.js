bd287851e0a30b299944df9141c1771f2b6cc047
amirhosseinlangari <amirhossseinlangari@gmail.com>
2025-06-04 12:08:13 +0330

    Update

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
 // ====== CONNECT TO MONGODB ======
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/kabancabinet', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: true
