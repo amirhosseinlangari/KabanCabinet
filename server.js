4d0a0e043071a48c093785e2ed7e979461764c0c
amirhosseinlangari <amirhossseinlangari@gmail.com>
2025-06-04 12:27:03 +0330

    update

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/test', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/kabancabinet')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB:', err));
 
